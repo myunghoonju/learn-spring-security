18c7dd25de2a3fccadadb948f952ef7001df10bf
myunghoonju <myunghoonju@gmail.com>
2021-09-20 00:04:01 +0900

    fix code style

--- src/main/java/org/learn/security/study/config/SecurityConfig.java
+++ src/main/java/org/learn/security/study/config/SecurityConfig.java
@@ -4,21 +4,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.authentication.logout.LogoutHandler;
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
 	private final UserDetailsService userDetailsService;
 
 	public SecurityConfig(UserDetailsService userDetailsService) {
@@ -31,20 +22,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.anyRequest().authenticated();
 		http
 				.formLogin();
+		//custom
+		//custom
 		http
 				.logout()
 				.logoutUrl("/logout") // post (default)
-				.addLogoutHandler(new LogoutHandler() { //custom
-					@Override public void logout(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) {
-						HttpSession session = httpServletRequest.getSession();
-						session.invalidate();
-					}
+				.addLogoutHandler((httpServletRequest, httpServletResponse, authentication) -> {
+					HttpSession session = httpServletRequest.getSession();
+					session.invalidate();
 				})
-				.logoutSuccessHandler(new LogoutSuccessHandler() { //custom
-					@Override public void onLogoutSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
-						httpServletResponse.sendRedirect("/login");
-					}
-				})
+				.logoutSuccessHandler((httpServletRequest, httpServletResponse, authentication) -> httpServletResponse.sendRedirect("/login"))
 				.and()
 				.rememberMe()
 				.rememberMeParameter("remember") // default "remember-me"
