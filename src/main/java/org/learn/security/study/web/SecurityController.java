bc7c84c4b31a17da917b29ca10a81f6bd3c54309
myunghoonju <myunghoonju@gmail.com>
2021-09-19 16:33:15 +0900

    initial commit

---
+++ src/main/java/org/learn/security/study/web/SecurityController.java
@@ -0,0 +1,14 @@
+package org.learn.security.study.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SecurityController {
+
+	@GetMapping("/")
+	public String index() {
+		return "hello";
+	}
+
+}
