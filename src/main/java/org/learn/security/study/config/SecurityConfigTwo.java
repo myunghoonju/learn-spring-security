ac78e6552cae285ae1e2a8e752f092ac9c345e46
myunghoonju <myunghoonju@gmail.com>
2021-09-20 16:52:21 +0900

    mark multi filter chains

--- src/main/java/org/learn/security/study/config/SecurityConfigTwo.java
+++ src/main/java/org/learn/security/study/config/SecurityConfigTwo.java
@@ -7,7 +7,7 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
+// multi filter chains
 @Configuration
 @EnableWebSecurity
 @Order(0)
